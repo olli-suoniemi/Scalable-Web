77d973077022f69e9c54a6d6e0487851627303ad
Olli Suoniemi <olli.suoniemi@aalto.fi>
2024-10-01 11:20:34 +0300

    edit

--- 24-project-II/qa-ui/astro.config.mjs
+++ 24-project-II/qa-ui/astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
   integrations: [svelte(), tailwind()],
+
   server: {
     port: 3000,
     host: true
   },
+
   output: 'server',
+  adapter: vercel(),
 });
